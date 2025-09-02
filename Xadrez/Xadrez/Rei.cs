364779980bc7f600f3ded7f6c91aa8cbcf5cfe7e
Mateus Paulino <80184052+MateusPaulino13@users.noreply.github.com>
2025-09-02 10:20:54 -0300

    adicionando movimentos possiveis Rei

--- Xadrez/Xadrez/Rei.cs
+++ Xadrez/Xadrez/Rei.cs
@@ -6,6 +6,60 @@ namespace Xadrez
     {
         public Rei(Cor cor, Tabuleiro tab) : base(cor, tab){}
 
+
+        private bool podeMover(Posicao pos)
+        {
+            Peca p = Tab.peca(pos);
+            return p == null || p.Cor != Cor;
+        }
+        public override bool[,] movimentosPossiveis()
+        {
+            bool[,] mat = new bool[Tab.linhas, Tab.colunas];
+            Posicao pos = new Posicao(0, 0);
+
+            // Acima
+            pos.definirValores(Posicao.Linha - 1, Posicao.Coluna);
+            if(Tab.posicaoValida(pos) && podeMover(pos))
+                mat[pos.Linha, pos.Coluna] = true;
+
+            // Abaixo
+            pos.definirValores(Posicao.Linha + 1, Posicao.Coluna);
+            if(Tab.posicaoValida(pos) && podeMover(pos))
+                mat[pos.Linha, pos.Coluna] = true;
+
+            // Direita
+            pos.definirValores(Posicao.Linha, Posicao.Coluna + 1);
+            if(Tab.posicaoValida(pos) && podeMover(pos))
+                mat[pos.Linha, pos.Coluna] = true;
+
+            // Esquerda
+            pos.definirValores(Posicao.Linha, Posicao.Coluna - 1);
+            if(Tab.posicaoValida(pos) && podeMover(pos))
+                mat[pos.Linha, pos.Coluna] = true;
+
+            // Nordeste
+            pos.definirValores(Posicao.Linha - 1, Posicao.Coluna + 1);
+            if(Tab.posicaoValida(pos) && podeMover(pos))
+                mat[pos.Linha, pos.Coluna] = true;
+
+            // Sudeste
+            pos.definirValores(Posicao.Linha + 1, Posicao.Coluna + 1);
+            if(Tab.posicaoValida(pos) && podeMover(pos))
+                mat[pos.Linha, pos.Coluna] = true;
+
+            // Sudoeste
+            pos.definirValores(Posicao.Linha + 1, Posicao.Coluna - 1);
+            if(Tab.posicaoValida(pos) && podeMover(pos))
+                mat[pos.Linha, pos.Coluna] = true;
+
+            // Noroeste
+            pos.definirValores(Posicao.Linha - 1, Posicao.Coluna - 1);
+            if(Tab.posicaoValida(pos) && podeMover(pos))
+                mat[pos.Linha, pos.Coluna] = true;
+
+            return mat;
+        }
+
         public override string ToString()
         {
             return "R";
