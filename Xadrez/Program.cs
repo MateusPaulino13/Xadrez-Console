9d2f5012d73d864f1adebcba13d2831a22424eb4
Mateus Paulino <80184052+MateusPaulino13@users.noreply.github.com>
2025-09-02 09:43:12 -0300

    adicionando  e implementando classe partida xadrez

--- Xadrez/Program.cs
+++ Xadrez/Program.cs
@@ -1,4 +1,5 @@
 ﻿using tabuleiro;
+using xadrez;
 
 namespace Xadrez
 {
@@ -8,17 +9,9 @@ namespace Xadrez
         {
             try
             {
-                Tabuleiro tab = new Tabuleiro(8, 8);
+                PartidaDeXadrez partida = new PartidaDeXadrez();
 
-                tab.colocarPeca(new Torre(Cor.Preta, tab), new Posicao(0, 0));
-                tab.colocarPeca(new Torre(Cor.Preta, tab), new Posicao(1, 3));
-                tab.colocarPeca(new Rei(Cor.Preta, tab), new Posicao(2, 4));
-
-                tab.colocarPeca(new Rei(Cor.Branca, tab), new Posicao(1, 0));
-                tab.colocarPeca(new Torre(Cor.Branca, tab), new Posicao(4, 2));
-                tab.colocarPeca(new Torre(Cor.Branca, tab), new Posicao(6, 3));
-
-                Tela.ImprimirTabuleiro(tab);
+                Tela.ImprimirTabuleiro(partida.tab);
             }
             catch (TabuleiroException e)
             {
