5557dd792b65b36c8e5b9e59fd7f4f66213c374f
Mateus Paulino <80184052+MateusPaulino13@users.noreply.github.com>
2025-09-01 23:02:21 -0300

    Adicionando classes essenciais Tabuleiro

---
+++ Xadrez/Tabuleiro/Peca.cs
@@ -0,0 +1,18 @@
+namespace tabuleiro
+{
+    class Peca
+    {
+        public Posicao Posicao { get; set; }
+        public Cor Cor { get; protected set; }
+        public int qteMovimentos { get; protected set; }
+        public Tabuleiro Tab { get; protected set; }
+
+        public Peca(Posicao posicao, Cor cor, Tabuleiro tab)
+        {
+            Posicao = posicao;
+            Cor = cor;
+            Tab = tab;
+            this.qteMovimentos = 0;
+        }
+    }
+}
