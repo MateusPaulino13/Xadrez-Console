4d998e590e687c557e5723854a7eb166c15824e0
Mateus Paulino <80184052+MateusPaulino13@users.noreply.github.com>
2025-09-02 08:34:37 -0300

    adicionando tratamento de execoes de tabuleiro

--- Xadrez/Tabuleiro/Tabuleiro.cs
+++ Xadrez/Tabuleiro/Tabuleiro.cs
@@ -1,3 +1,6 @@
+using System.ComponentModel.DataAnnotations;
+using tabuleiro;
+
 namespace tabuleiro
 {
     class Tabuleiro
@@ -18,10 +21,37 @@ namespace tabuleiro
             return pecas[linha, coluna];
         }
 
+        public Peca peca(Posicao pos)
+        {
+            return pecas[pos.Linha, pos.Coluna];
+        }
+
         public void colocarPeca(Peca p, Posicao pos)
         {
+            if(existePeca(pos))
+                throw new TabuleiroException("Já existe uma peça nessa posição!");
+
             pecas[pos.Linha, pos.Coluna] = p;
             p.Posicao = pos;
+        }
+
+        public bool existePeca(Posicao pos)
+        {
+            validarPosicao(pos);
+            return peca(pos) != null;
+        }
+
+        public bool posicaoValida(Posicao pos)
+        {
+            if (pos.Linha < 0 || pos.Linha >= linhas || pos.Coluna < 0 || pos.Coluna >= colunas)
+                return false;
+            return true;
+        }
+
+        public void validarPosicao(Posicao pos)
+        {
+            if(!posicaoValida(pos))
+                throw new TabuleiroException("Posição inválida!");
         }
     }
 }
